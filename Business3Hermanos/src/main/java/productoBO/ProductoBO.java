ef7742119757f200a1f623561034d4c74164d941
Nube28 <159978169+Nube28@users.noreply.github.com>
2025-05-06 19:43:19 -0700

    Creacion de productoBO

---
+++ Business3Hermanos/src/main/java/productoBO/ProductoBO.java
@@ -0,0 +1,15 @@
+package productoBO;
+
+/**
+ *
+ * Clase con los metodos necesarios para Gestionar el producto
+ *
+ * @author Alejandro Gómez Vega 00000247313
+ * @author Francisco Valdez Gastelum 00000246904
+ * @author Kathya Margarita Cordova Soto 00000246801
+ * @author Manuel Octavio Perez Dominguez 00000247439
+ * @author Adán Eduardo Cornejo Balcázar 000000228558
+ */
+public class ProductoBO {
+    
+}
